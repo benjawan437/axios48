1b6bb47e1b4ffa5c4fe57382df1a9dc024889a45
benjawn497 <benjawn497@gmail.com>
2020-10-03 15:57:25 +0700

    nn

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="app">
-    <h1 class="ti" >The Movie</h1>
-    <img alt="Vue logo" src="./assets/l2.png" />
+  <div align="center">
+    <div id="app" class="bx">
+      <h1 class="ti">The Anime</h1>
+      <h5 class="ro">The center of anime and manga information around the world.</h5>
+      <img alt="Vue logo" src="./assets/hl1.png" />
+    </div>
     <anime />
   </div>
 </template>
@@ -25,8 +28,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.ti{
+.ti {
   color: rgb(251, 251, 247);
   font-size: 60px;
+}
+.ro{
+  color: white;
+}
+.bx {
+  background-color:rgba(48, 97, 201, 0.68);
+  width: 900px;
+  padding: 20px;
+  margin: 25px;
 }
 </style>
--- src/components/Anime.vue
+++ src/components/Anime.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="contsiner">
-    <div align="center">
-      <b-form-input
-        v-model="keyword"
-        class="sea"
-        placeholder="Enter a movie title"
-      ></b-form-input>
-        <b-button pill variant="warning" @click="search()">Search</b-button>
-    </div>
-    <b-card-group class="mt-2" columns>
+      <div align="center">
+        <b-form-input
+          v-model="keyword"
+          class="sea"
+          placeholder="Enter a movie title"
+        ></b-form-input>
+        <b-button pill variant="outline-dark" @click="searchData()"
+          >Search Anime</b-button
+        >
+      </div>
+
+    <b-card-group columns>
       <b-card
-        class="mb-2"
         v-for="data in result"
-        :key="data.imdbID"
-        :title="data.Title"
-        :img-src="data.Poster"
+        :key="data.mal_id"
+        :title="data.title"
+        :img-src="data.image_url"
         :img-alt="Image"
         img-top
         tag="article"
         style="max-width: 20rem"
+        class="mb-2"
       >
+        <b-card-text>{{ data.synopsis }}</b-card-text>
+        <b-button :href="data.url" variant="primary">Go somewhere</b-button>
       </b-card>
     </b-card-group>
   </div>
 </template>
-
 <script>
 import axios from "axios";
 export default {
@@ -32,25 +36,20 @@ export default {
     return {
       result: null,
       keyword: "",
-      data: "",
     };
   },
   methods: {
-    search() {
+    searchData() {
       axios
-        .get("http://www.omdbapi.com/?s=" + this.keyword + "&apikey=409a3997")
+        .get(
+          "https://api.jikan.moe/v3/search/anime?q=" + this.keyword + "&page=1"
+        )
         .then((response) => {
-          this.result = response.data.Search;
-          console.log(this.result);
+          this.result = response.data.results;
         })
-        .catch((err) => console.log(err));
-    },
-  },
-  watch: {
-    searchmet: function (val) {
-      if (val == 3) {
-        this.upcoming();
-      }
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
 };
